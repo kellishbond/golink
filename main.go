e131eb9dc328e6f71d932bc4d70c69383f076c89
unknown <kellishbond@gmail.com>
2026-06-11 16:41:15 +0100

    day 1: basic HTTP server and health check endpoint

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/kellishbond/golink/internal/handler"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health", handler.Health)
+
+	fmt.Println("GoLink server running on :8080")
+	http.ListenAndServe(":8080", mux)
+}
