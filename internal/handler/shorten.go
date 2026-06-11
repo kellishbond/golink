ba8fb28312cf3d097ca260caf0b911e4524edaf5
unknown <kellishbond@gmail.com>
2026-06-11 17:13:17 +0100

    day 2: POST /shorten endpoint with code generation

---
+++ internal/handler/shorten.go
@@ -0,0 +1,48 @@
+package handler
+
+import (
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+var store = map[string]string{}
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+func generateCode() string {
+	rand.Seed(time.Now().UnixNano())
+	code := make([]rune, 6)
+	for i := range code {
+		code[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(code)
+}
+
+func Shorten(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var body struct {
+		URL string `json:"url"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil || body.URL == "" {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	code := generateCode()
+	store[code] = body.URL
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]string{
+		"code":      code,
+		"short_url": "http://localhost:8080/" + code,
+	})
+}
--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-
 	mux.HandleFunc("/health", handler.Health)
+	mux.HandleFunc("/shorten", handler.Shorten)
 
 	fmt.Println("GoLink server running on :8080")
 	http.ListenAndServe(":8080", mux)
